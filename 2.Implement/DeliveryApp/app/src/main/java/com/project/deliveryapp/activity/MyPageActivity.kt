888bbafc1d4433dee0cf568a6e91dd69981f31f2
Rure00 <asnjkd@naver.com>
2023-08-07 17:44:34 +0900

    ERD 수정
    add 'review' Table

---
+++ 2.Implement/DeliveryApp/app/src/main/java/com/project/deliveryapp/activity/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.project.deliveryapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.deliveryapp.R
+
+class MyPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
---
+++ 2.Implement/DeliveryApp/app/src/main/java/com/project/deliveryapp/activity/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.project.deliveryapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.deliveryapp.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
