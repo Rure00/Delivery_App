9fe2b78ed486e61a80e6394933c5344ef8df81a7
Rure00 <asnjkd@naver.com>
2023-09-18 16:34:24 +0900

    Activity 연결
    kotlin 공부하고 다시 시작

--- 2.Implement/DeliveryApp/app/src/main/java/com/project/deliveryapp/activity/SignInActivity.kt
+++ 2.Implement/DeliveryApp/app/src/main/java/com/project/deliveryapp/activity/SignInActivity.kt
@@ -1,12 +1,52 @@
 package com.project.deliveryapp.activity
 
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import com.project.deliveryapp.R
+import com.project.deliveryapp.databinding.ActivitySignInBinding
+import kotlin.math.sign
 
 class SignInActivity : AppCompatActivity() {
+
+    private var viewBinding: ActivitySignInBinding? = null
+    private val binding get() = viewBinding!!
+
+    private lateinit var idText: EditText
+    private lateinit var pwText: EditText
+    private lateinit var signUpBtn: Button
+    private lateinit var signInBtn: Button
+    private lateinit var checkSignIn: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        viewBinding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        idText = binding.idText
+        pwText = binding.pwText
+        signInBtn = binding.signInBtn
+        signUpBtn = binding.signUpBtn
+        checkSignIn = binding.checkbox
+
+        signInBtn.setOnClickListener {
+
+        }
+        signUpBtn.setOnClickListener {
+
+        }
+        checkSignIn.setOnCheckedChangeListener { view, isChecked ->
+            val result = when(isChecked) {
+                true -> 1
+                false -> 2
+            }
+        }
+
     }
+
+
 }
---
+++ 2.Implement/DeliveryApp/app/src/main/java/com/project/deliveryapp/data/UserData.kt
@@ -0,0 +1,10 @@
+package com.project.deliveryapp.data
+
+data class UserData(
+    val name: String,
+    val nickname: String,
+    val loginId: String,
+    val loginPwd: String,
+    val phoneNumber: String,
+    val gender
+)
